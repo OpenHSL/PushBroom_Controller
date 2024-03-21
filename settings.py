c4ccf223778f0a0772cfa49b5b561c3d166315d8
OpenHSL <119412883+OpenHSL@users.noreply.github.com>
2024-03-21 17:49:43 +0400

    Update settings.py
    upd

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 
 class CameraSettings:
-    number_of_steps = 10,
-    exposure = 1_000_000,
-    gain = 0,
+    number_of_steps = 10
+    exposure = 1_000_000
+    gain = 0
     direction = 0
     path_to_save = "./out"
     mode = 0
