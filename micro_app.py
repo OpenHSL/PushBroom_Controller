cb507b679148c5d2bd2e6764f082b41283a4cd86
Liptee <muzzone777@gmail.com>
2024-03-22 14:24:49 -0700

    add gui

---
+++ micro_app.py
@@ -0,0 +1,101 @@
+import sys
+from queue import Queue
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QThread, QObject, pyqtSignal as Signal, pyqtSlot as Slot
+from PyQt5.QtGui import QPixmap
+
+import utils
+from gui.common_gui import CIU
+from gui.mac_micro_gui import Ui_MainWindow
+from settings import CameraSettings
+import main as controller
+
+shots_buffer = Queue()
+sets = CameraSettings()
+camera, servomotor = controller.init_hardware()
+
+camera.set_camera_configures(exposure=sets.exposure,
+                             gain=sets.gain)
+
+servomotor.initialize_pins(direction=sets.direction,
+                           mode=sets.mode)
+
+
+class Worker(QObject):
+    global camera
+    global servomotor
+    meta_data = Signal(dict)
+
+    @Slot(dict)
+    def do_work(self, meta):
+        try:
+            print(meta)
+            controller.start_record(camera=camera,
+                                    servomotor=servomotor,
+                                    number_of_steps=meta.number_of_steps,
+                                    path_to_save=meta.path_to_save)
+            self.meta_data.emit({"Status": "Done"})
+        except Exception as e:
+            self.meta_data.emit({"Status": "Error", "Error": str(e)})
+
+
+class MainWindow(CIU):
+    meta_requested = Signal(CameraSettings)
+
+    def __init__(self):
+        CIU.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.show()
+
+        # THREAD SETUP
+        self.worker = Worker()
+        self.worker_thread = QThread()
+
+        self.worker.meta_data.connect(self.end_record)
+        self.meta_requested.connect(self.worker.do_work)
+
+        self.worker.moveToThread(self.worker_thread)
+        self.worker_thread.start()
+
+        # BUTTONS CONNECTIONS
+        self.ui.start_btn.clicked.connect(self.start_record)
+        self.ui.preview_btn.clicked.connect(self.make_shot)
+
+    def make_shot(self):
+        global camera
+
+        sets.exposure = int(self.ui.exposure_edit.text())
+        sets.gain = int(self.ui.gain_edit.text())
+
+        camera.set_camera_configures(exposure=sets.exposure,
+                                     gain=sets.gain)
+
+        image = camera.make_shot()
+        q_image = self.nparray_2_qimage(image)
+        self.ui.image_label.setPixmap(QPixmap(q_image))
+
+    def end_record(self, status):
+        if status["Status"] == "Done":
+            self.ui.start_btn.setEnabled(True)
+        else:
+            self.ui.start_btn.setEnabled(True)
+            self.show_error(status["Error"])
+
+    def start_record(self):
+        self.ui.start_btn.setEnabled(False)
+
+        sets.exposure = int(self.ui.exposure_edit.text())
+        sets.gain = int(self.ui.gain_edit.text())
+        sets.direction = int(self.ui.direction_edit.text())
+        sets.mode = int(self.ui.mode_edit.text())
+        sets.number_of_steps = int(self.ui.step_edit.text())
+
+        self.meta_requested.emit(sets)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
